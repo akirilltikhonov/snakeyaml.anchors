820c369f859108567b24463cebfc18b940327f41
Kirill Tikhonov <Kirill.Tikhonov@webfleet.com>
2024-09-26 11:48:00 +0200

    init commit

---
+++ src/main/java/snakeyaml/anchors/AnchorsApplication.java
@@ -0,0 +1,13 @@
+package snakeyaml.anchors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnchorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnchorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/snakeyaml/anchors/AnchorsApplicationTests.java
@@ -0,0 +1,13 @@
+package snakeyaml.anchors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
